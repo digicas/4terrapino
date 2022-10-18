c66697f047e539fb4123c9895e90e48d9492411b
Daniel Klein <daniel.klein@develit.io>
2022-10-18 11:34:15 +0200

    test

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -17,7 +17,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        appBarTheme: const AppBarTheme(color: Colors.red),
         colorScheme: ColorScheme.fromSwatch(
           accentColor: const Color(0xFF13B9FF),
         ),
