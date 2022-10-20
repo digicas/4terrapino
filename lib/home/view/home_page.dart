fd5d4b15fca0b469bc67c33cd062dc6092509efd
Daniel Klein <daniel.klein@develit.io>
2022-10-20 21:29:04 +0200

    !!

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -5,9 +5,15 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+// ignore_for_file: avoid_web_libraries_in_flutter
+
+import 'dart:developer';
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:terrapino/home/home.dart';
+import 'package:terrapino/models/activity_event.dart';
 import 'package:terrapino/models/uri_result.dart';
 
 class HomePage extends StatelessWidget {
@@ -26,6 +32,33 @@ class HomePage extends StatelessWidget {
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
+
+  void handleOnLevelStartedEvent(TaskType type, int level) {
+    log('Level started event');
+
+    final eventDataJson = ActivityEvent(
+      event: ActivityEventType.levelStarted,
+      type: type,
+      levelNumber: level,
+    ).toJson();
+
+    context.callMethod(
+      'onEvent',
+      [eventDataJson],
+    );
+  }
+
+  void handleOnLevelFinishedEvent(TaskType type, int level) {
+    log('Level finished event');
+    final eventDataJson = ActivityEvent(
+      event: ActivityEventType.levelStarted,
+      type: type,
+      levelNumber: level,
+      seconds: 9200,
+      tries: 3,
+    ).toJson();
+    context.callMethod('onEvent', [eventDataJson]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,45 +80,56 @@ class HomeView extends StatelessWidget {
                   (HomeCubit cubit) => 'level: ${cubit.state.level}',
                 ),
               ),
+              const SizedBox(height: 32),
+              BlocBuilder<HomeCubit, UriResult>(
+                builder: (context, state) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      EventButton(
+                        title: 'Level Started',
+                        onPressed: () => handleOnLevelStartedEvent(
+                          TaskType.fromString(state.type),
+                          state.level,
+                        ),
+                      ),
+                      const SizedBox(width: 32),
+                      EventButton(
+                        title: 'Level Finished',
+                        onPressed: () => handleOnLevelFinishedEvent(
+                          TaskType.fromString(state.type),
+                          state.level,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
             ],
           ),
         ),
       ),
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   final l10n = context.l10n;
-  //   return Scaffold(
-  //     appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
-  //     body: const Center(child: HomeText()),
-  //     floatingActionButton: Column(
-  //       mainAxisAlignment: MainAxisAlignment.end,
-  //       crossAxisAlignment: CrossAxisAlignment.end,
-  //       children: [
-  //         FloatingActionButton(
-  //           onPressed: () => context.read<HomeCubit>().increment(),
-  //           child: const Icon(Icons.add),
-  //         ),
-  //         const SizedBox(height: 8),
-  //         FloatingActionButton(
-  //           onPressed: () => context.read<HomeCubit>().decrement(),
-  //           child: const Icon(Icons.remove),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 }
 
-class HomeText extends StatelessWidget {
-  const HomeText({super.key});
+class EventButton extends StatelessWidget {
+  const EventButton({
+    super.key,
+    required this.title,
+    this.onPressed,
+  });
+
+  final String title;
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final count = context.select((HomeCubit cubit) => cubit.state);
-    return Text('$count', style: theme.textTheme.headline1);
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        title,
+      ),
+    );
   }
 }
