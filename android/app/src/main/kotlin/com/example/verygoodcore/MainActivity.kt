9558612bccb6e44c379d9ae957bb62510a6c501c
Daniel Klein <daniel.klein@develit.io>
2022-10-21 10:54:30 +0200

    refact: remove unused platforms

--- android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.verygoodcore.terrapino
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/activity_event.dart
+++ lib/models/activity_event.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first, no_default_cases
+// ignore_for_file: public_member_api_docs, sort_constructors_first, no_default_cases, avoid_web_libraries_in_flutter
 import 'dart:convert';
 import 'dart:js';
 
